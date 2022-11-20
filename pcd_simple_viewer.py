ca8910c77b8c8e7615910b870071c3c730c9154e
Tae wook <laputa99999@gmail.com>
2022-11-20 19:40:49 +0900

    Update pcd_simple_viewer.py

--- pcd_simple_viewer.py
+++ pcd_simple_viewer.py
@@ -3,6 +3,7 @@
 import copy
 import numpy as np
 import open3d as o3d
+import argparse, readline
 
 cmap = np.array([[1.00, 1.00, 1.00], [0.00, 0.00, 0.00], \
                     [1.00, 0.00, 0.00], [1.00, 0.00, 0.40], \
@@ -15,16 +16,18 @@ cmap = np.array([[1.00, 1.00, 1.00], [0.00, 0.00, 0.00], \
                     [0.60, 0.80, 0.40], [1.00, 0.60, 0.80]],'f')
 
 if __name__ == "__main__":
-    # file = "/home/ktw/projects/randla-net-tf2/test/Log_2022-11-05_13-35-40/predictions/UNF1.labels"
-    # file = "/home/ktw/projects/randla-net-tf2/test/Log_2022-11-05_13-35-40/predictions/birdfountain1.labels"
-    file = "/home/ktw/projects/randla-net-tf2/test/Log_2022-11-05_13-35-40/predictions/castleblatten1.labels"
-    label = np.loadtxt(file)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_pcd', type=str, required=True)    # pcd format is txt. xyzrgb
+    parser.add_argument('--input_label', type=str, required=True)
+    args = parser.parse_args() 
+    
+    label = np.loadtxt(args.input_label)
     label = label.astype(np.int32)
 
     # print('label')
     # print(cmap[label])
 
-    pcd_origin = np.loadtxt("/home/ktw/data/semantic3d/original_data/castleblatten_station1_intensity_rgb.txt", delimiter=' ')
+    pcd_origin = np.loadtxt(args.input_pcd, delimiter=' ')
     pcd = o3d.geometry.PointCloud()
     # pcd.points = pcd_origin
     pcd.points = o3d.utility.Vector3dVector(pcd_origin[:,0:3]) # XYZ points
